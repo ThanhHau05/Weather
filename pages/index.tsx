a9f719f6e7bb7c5494263a01aa887d440d7e0cd0
ThanhHau05 <nguyenthanhhau.contact@gmail.com>
2023-05-26 20:13:57 +0700

    update api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import classNames from "classnames/bind";
+import { SetStateAction, useEffect, useState } from "react";
+import {
+  AiFillCaretDown,
+  AiFillCaretUp,
+  AiOutlineLoading3Quarters,
+} from "react-icons/ai";
 import Images from "../public/Images";
 import styles from "../styles/Home.module.scss";
-import { useEffect, useState } from "react";
-import { AiFillCaretDown, AiFillCaretUp, AiOutlineLoading3Quarters } from "react-icons/ai";
 
-import list_item from "../components/ListItem";
 import { useOnClickOut } from "../Hooks/useOnClickOut";
+import list_item from "../components/ListItem";
 import { removeVietnameseTones } from "../components/VNTones";
 const axios = require("axios");
 const cx = classNames.bind(styles);
@@ -71,8 +75,11 @@ export default function Home() {
     if (dataitem !== undefined && dataitem.length === 4) {
       return dataitem.map((item, index) => (
         <div key={index} className={cx("forecast-day-container")}>
-          
-          {item.day === "CN" ? <span>{item.day}</span> : <span>Thứ {item.day}</span>}
+          {item.day === "CN" ? (
+            <span>{item.day}</span>
+          ) : (
+            <span>Thứ {item.day}</span>
+          )}
           <img src={item.icon} alt="" />
           <span className={cx("forecast-day-avg")}>{item.avgtemp_c}°</span>
         </div>
@@ -80,8 +87,15 @@ export default function Home() {
     } else {
       return [1, 2, 3, 4].map((item, index) => (
         <div key={index}>
-          <div className={cx("loading-icon-container", "loading-icon-forecast-container")}>
-            <AiOutlineLoading3Quarters className={cx("loading-icon", "loading-icon-forecast")} />
+          <div
+            className={cx(
+              "loading-icon-container",
+              "loading-icon-forecast-container"
+            )}
+          >
+            <AiOutlineLoading3Quarters
+              className={cx("loading-icon", "loading-icon-forecast")}
+            />
           </div>
         </div>
       ));
@@ -90,7 +104,6 @@ export default function Home() {
 
   const _handleRenderItemOption = () => {
     if (valueinput === "") {
-      
       return list_item.LIST_PROVINCE.map((item, index) => (
         <div
           key={index}
@@ -140,11 +153,13 @@ export default function Home() {
   useEffect(() => {
     if (selecttitle !== "") {
       const province = removeVietnameseTones(selecttitle);
-      const API_KEY = "4dea35c3744c4d2ca6d93100220909";
+      const API_KEY = "b718dfd9ede8433290f125849232605";
       const BASE_URL = "//api.weatherapi.com";
-      axios.get(`${BASE_URL}/v1/current.json?key=${API_KEY}&lang=vi&q=${province}`).then((response) => {
-        setWeather(response.data);
-      });
+      axios
+        .get(`${BASE_URL}/v1/current.json?key=${API_KEY}&lang=vi&q=${province}`)
+        .then((response: { data: SetStateAction<WeatherAPI> }) => {
+          setWeather(response.data);
+        });
     }
   }, [selecttitle]);
 
@@ -165,11 +180,13 @@ export default function Home() {
 
   const text = async () => {
     if (selecttitle !== "") {
-      const [DateToday, MonthToday, YearToday] = dates.toLocaleDateString().split("/");
+      const [DateToday, MonthToday, YearToday] = dates
+        .toLocaleDateString()
+        .split("/");
       const day = new Date(`${MonthToday} ${DateToday}, ${YearToday}`);
       const nextDay = new Date(day);
       const days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-      const API_KEY = "4dea35c3744c4d2ca6d93100220909";
+      const API_KEY = "b718dfd9ede8433290f125849232605";
       const BASE_URL = "//api.weatherapi.com";
       const dataitems = [];
       for (let i = 1; i <= 4; i++) {
@@ -183,7 +200,9 @@ export default function Home() {
         }
         const province = removeVietnameseTones(selecttitle);
         await axios
-          .get(`${BASE_URL}/v1/forecast.json?key=${API_KEY}&dt=${textDay[3]}-${textDay[1]}-${textDay[2]}&q=${province}`)
+          .get(
+            `${BASE_URL}/v1/forecast.json?key=${API_KEY}&dt=${textDay[3]}-${textDay[1]}-${textDay[2]}&q=${province}`
+          )
           .then((response) => {
             const item = {
               day: dayweek,
@@ -216,7 +235,10 @@ export default function Home() {
           <img className={cx("img-main")} src={Images.Main.src} alt="" />
           <div className={cx("main-container")}>
             <div ref={countRef} className={cx("select-container")}>
-              <div className={cx("select-title-container")} onClick={() => _handleOpenOptionItem()}>
+              <div
+                className={cx("select-title-container")}
+                onClick={() => _handleOpenOptionItem()}
+              >
                 <span className={cx("select-title")}>{selecttitle}</span>
                 {!iconup ? (
                   <AiFillCaretDown className={cx("select-icon")} />
@@ -237,7 +259,8 @@ export default function Home() {
             </div>
             <div className={cx("day-time-container")}>
               <p>
-                Thứ {dates.getDay()} Ngày {dates.getDate()} Tháng {dates.getMonth()} - {hour}:{minute} {AMorPM}
+                Thứ {dates.getDay()} Ngày {dates.getDate()} Tháng{" "}
+                {dates.getMonth()} - {hour}:{minute} {AMorPM}
               </p>
             </div>
             <div className={cx("province-title")}>
@@ -259,12 +282,16 @@ export default function Home() {
             <span className={cx("province-text")} style={{ fontWeight: "500" }}>
               {weather?.current.condition.text}
             </span>
-            <span className={cx("province-text")}>CẢM THẤY NHƯ {weather?.current.feelslike_c}°</span>
+            <span className={cx("province-text")}>
+              CẢM THẤY NHƯ {weather?.current.feelslike_c}°
+            </span>
           </div>
           <div className={cx("nextdayforecast")}>
             <div className={cx("nextdayforecast-wrapper")}>
               <h2>Nhiệt độ trung bình</h2>
-              <div className={cx("nextdayforecast-container")}>{_handleRenderItemForecast()}</div>
+              <div className={cx("nextdayforecast-container")}>
+                {_handleRenderItemForecast()}
+              </div>
             </div>
           </div>
         </div>
